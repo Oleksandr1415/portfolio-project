0e3826ee8ba453b7edafdb86cd5074fa3dc1e027
Oleksandr Pustovit <oleksandr.pustovit@nauten.io>
2026-07-20 13:09:21 +0200

    Add DDEV configuration.

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,9 +7,16 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+    server: {
+    host: true // binds to 0.0.0.0
+  },
+  
   integrations: [react()],
 
   vite: {
+    server: {
+      allowedHosts: ["portfolio-project.ddev.site"],
+    },
     plugins: [tailwindcss()]
   }
 });
