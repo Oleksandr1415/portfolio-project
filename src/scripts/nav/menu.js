593f27cbd395125dbfa606451297d364547360e0
Oleksandr Pustovit <oleksandr.pustovit@nauten.io>
2026-07-23 13:49:37 +0200

    finished overall menu functionality.

--- src/scripts/nav/menu.js
+++ src/scripts/nav/menu.js
@@ -1,9 +1,22 @@
-const menuButton = document.querySelector('[data-slot="toggleMenu"]');
+const menuButton = document.querySelector('[data-slot="toggle-nav-menu"]');
+const menuBackground = document.querySelector(
+	'[data-slot="nav-menu-background"]',
+);
 const openMenu = document.getElementById("nav-menu-burger-icon");
 const closeMenu = document.getElementById("nav-menu-close-icon");
+const navMenu = document.getElementById("navigation-planet");
+const navMenuItems = document.querySelectorAll('[data-slot="menu-item"');
 
-menuButton?.addEventListener("click", () => {
+menuButton?.addEventListener("click", toggleMenu);
+menuBackground.addEventListener("click", toggleMenu);
+navMenuItems.forEach((item) => {
+	item.addEventListener("click", toggleMenu);
+});
+
+function toggleMenu() {
 	openMenu.classList.toggle("opacity-0");
 	closeMenu.classList.toggle("opacity-0");
 	closeMenu.classList.toggle("rotate-90");
-});
+	navMenu.classList.toggle("translate-y-[-70%]");
+	menuBackground.classList.toggle("-translate-y-full");
+}
