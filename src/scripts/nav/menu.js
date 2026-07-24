a809982a4a9cb2e0c85b2f827f85239f0acb6936
Oleksandr Pustovit <oleksandr.pustovit@nauten.io>
2026-07-24 10:24:30 +0200

    Set prettier settings. Added plugins for Tailwind and Astro. Formatted all files accordingly

--- src/scripts/nav/menu.js
+++ src/scripts/nav/menu.js
@@ -1,22 +1,20 @@
 const menuButton = document.querySelector('[data-slot="toggle-nav-menu"]');
-const menuBackground = document.querySelector(
-	'[data-slot="nav-menu-background"]',
-);
-const openMenu = document.getElementById("nav-menu-burger-icon");
-const closeMenu = document.getElementById("nav-menu-close-icon");
-const navMenu = document.getElementById("navigation-planet");
+const menuBackground = document.querySelector('[data-slot="nav-menu-background"]');
+const openMenu = document.getElementById('nav-menu-burger-icon');
+const closeMenu = document.getElementById('nav-menu-close-icon');
+const navMenu = document.getElementById('navigation-planet');
 const navMenuItems = document.querySelectorAll('[data-slot="menu-item"');
 
-menuButton?.addEventListener("click", toggleMenu);
-menuBackground.addEventListener("click", toggleMenu);
+menuButton?.addEventListener('click', toggleMenu);
+menuBackground.addEventListener('click', toggleMenu);
 navMenuItems.forEach((item) => {
-	item.addEventListener("click", toggleMenu);
+  item.addEventListener('click', toggleMenu);
 });
 
 function toggleMenu() {
-	openMenu.classList.toggle("opacity-0");
-	closeMenu.classList.toggle("opacity-0");
-	closeMenu.classList.toggle("rotate-90");
-	navMenu.classList.toggle("translate-y-[-70%]");
-	menuBackground.classList.toggle("-translate-y-full");
+  openMenu.classList.toggle('opacity-0');
+  closeMenu.classList.toggle('opacity-0');
+  closeMenu.classList.toggle('rotate-90');
+  navMenu.classList.toggle('translate-y-[-70%]');
+  menuBackground.classList.toggle('-translate-y-full');
 }
