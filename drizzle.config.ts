7ad62dab73d6eeb636fd027e66aeeb44916bfeac
Oleksandr Pustovit <oleksandr.pustovit@nauten.io>
2026-08-27 16:43:31 +0200

    [Fix] Fixed minor issues. Error in tsconfig.json. Unused font imports in global.css file

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,8 +2,8 @@ import { defineConfig } from 'drizzle-kit';
 import 'dotenv/config';
 
 export default defineConfig({
-  schema: './src/lib/schema.ts',
   out: './drizzle',
+  schema: './src/lib/schema.ts',
   dialect: 'postgresql',
   dbCredentials: {
     url: process.env.DATABASE_URL!,
